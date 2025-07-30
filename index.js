1c99b3950bdaf58f9fd3321021737cd1c97b2716
5jun99 <oj041088@gmail.com>
2025-07-30 11:08:00 +0900

    feat: CORS 설정

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const { swaggerUi, specs } = require("./config/swagger");
+
 const app = express();
 const PORT = 3000;
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use(
+    cors({
+        origin: process.env.CLIENT_ORIGIN,
+        credentials: true,
+    })
+);
 
 app.get("/", (req, res) => {
     res.send("민원 음성 도우미 서버가 실행 중입니다.");
